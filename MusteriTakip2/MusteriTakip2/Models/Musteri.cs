c51da1d590411758bb7748cf7770cc7785cae29d
GarfieldElf <elfaycicekk@gmail.com>
2024-03-12 03:39:00 +0300

    delete data and load

---
+++ MusteriTakip2/MusteriTakip2/Models/Musteri.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusteriTakip2.Models
+{
+    public class Musteri : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        }
+
+         //[AutoIncrement]
+        private int musterid { get; set; }
+        private string musteriadi { get; set; } = string.Empty;
+        private string musterisoyadi { get; set; } = string.Empty;
+        private string musteritel { get; set; } = string.Empty;
+        private string musterieposta { get; set; } = string.Empty;
+
+        public int MusteriId
+        {
+            get { return musterid; }
+            set { musterid = value; OnPropertyChanged("MusteriId"); }
+        }
+
+        public string MusteriAdi
+        {
+            get { return musteriadi; }
+            set { musteriadi = value; OnPropertyChanged("MusteriAdi"); }
+        }
+
+        public string MusteriSoyadi
+        {
+            get { return musterisoyadi; }
+            set { musterisoyadi = value; OnPropertyChanged("MusteriSoyadi"); }
+        }
+
+        public string MusteriTel
+        {
+            get { return musteritel; }
+            set { musteritel = value; OnPropertyChanged("MusteriTel"); }
+        }
+
+        public string MusteriEposta
+        {
+            get { return musterieposta; }
+            set { musterieposta = value; OnPropertyChanged("MusteriEposta"); }
+        }
+
+
+    }
+}
---
+++ MusteriTakip2/MusteriTakip2/ViewModels/MusteriViewModel.cs
@@ -0,0 +1,91 @@
+﻿using MusteriTakip2.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MusteriTakip2.ViewModels
+{
+    public class MusteriViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        }
+
+        private Musteriler _musteriler;
+
+        public Musteriler Musteriler
+        {
+            get { return _musteriler; }
+            set { _musteriler = value; OnPropertyChanged(nameof(Musteriler)); }
+        }
+
+        private ObservableCollection<Musteriler> _lstmusteri;
+
+        public ObservableCollection<Musteriler> LstMusteri
+        {
+            get { return _lstmusteri; }
+            set { _lstmusteri = value; OnPropertyChanged(nameof(LstMusteri)); }
+        }
+
+        MusteriEntities musterientities;
+
+        public MusteriViewModel()
+        {
+            musterientities = new MusteriEntities();
+            LoadMusteri();
+            DeleteCommand = new Command((s) => true, Delete);
+        }
+
+        private void Delete(object obj)
+        {
+            var emp = obj as Musteriler;
+            musterientities.Musterilers.Remove(emp);
+            musterientities.SaveChanges();
+            LstMusteri.Remove(emp);
+        }
+
+        private void LoadMusteri()
+        {
+            LstMusteri = new ObservableCollection<Musteriler>(musterientities.Musterilers);
+        }
+        public ICommand DeleteCommand { get; set; }
+
+        class Command : ICommand
+        {
+            public Command(Func<object,bool> methodCanExecute,Action<object> methodExecute)
+            {
+                MethodCanExecute = methodCanExecute;
+                MethodExecute = methodExecute;
+            }
+
+            Action<object> MethodExecute;
+            Func<object, bool> MethodCanExecute;
+            public event EventHandler CanExecuteChanged;
+
+            public bool CanExecute(object parameter)
+            {
+                return MethodExecute != null && MethodCanExecute.Invoke(parameter);
+            }
+
+            public void Execute(object parameter)
+            {
+                MethodExecute(parameter);
+            }
+        }
+
+    }
+
+
+
+}
